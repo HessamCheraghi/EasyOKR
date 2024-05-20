712619df607874b997eb010a3eb8b8d410d70e92
HessamCheraghi <hessam.ch.rose@gmail.com>
2024-05-21 02:10:39 +0330

    change: project name

--- src/components/container/Title.tsx
+++ src/components/container/Title.tsx
@@ -2,10 +2,10 @@ import { Goal } from "lucide-react";
 
 export function Title() {
   return (
-    <div className="flex h-16 md:h-14 items-center border-b pr-4 md:px-4 lg:h-[60px] lg:px-6">
+    <div className="flex h-16 items-center border-b pr-4 md:h-14 md:px-4 lg:h-[60px] lg:px-6">
       <a href="/" className="flex items-center gap-2 font-semibold">
         <Goal className="h-6 w-6" />
-        <span className="">Goals Project</span>
+        <span className="">Easy OKR</span>
       </a>
     </div>
   );
